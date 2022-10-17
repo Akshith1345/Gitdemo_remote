f972b815159dd7fa0ba1f9838cb2eae4ac7874bc
Akshith1345 <akshith.kumar@terralogic.com>
2022-10-17 14:26:24 +0530

    CostomeLable

--- Gitdemo/ViewController.swift
+++ Gitdemo/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var lable1: UILabel!
+    
+    var costumeLable = UILabel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -17,6 +20,13 @@ class ViewController: UIViewController {
         lable1.backgroundColor = .green
         lable1.text = "hey My name is Akshith"
         
+        
+        costumeLable.frame = CGRect(x: 50, y: 50, width: 100, height: 100)
+        costumeLable.text = "Hello World"
+        costumeLable.numberOfLines = 3
+        view.addSubview(costumeLable)
+        costumeLable.backgroundColor = .white
+        costumeLable.textAlignment = .center
         
     }
 
