1a4e7a614b2cfd7ca0a5081f3db0d6f8d7706a98
Akshith1345 <akshith.kumar@terralogic.com>
2022-10-17 12:36:31 +0530

    working

--- Gitdemo/ViewController.swift
+++ Gitdemo/ViewController.swift
@@ -9,10 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lable1: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         view.backgroundColor = .red
+        lable1.backgroundColor = .green
+        lable1.text = "hey My name is Akshith"
         
         
     }
